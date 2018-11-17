e49b32aba498d656816376e425fa3dea23b520ea
ilhamhanif15 <ilhamhanif1512@gmail.com>
2018-11-17 16:42:46 +0700

    Update application/views/landing_page.php

--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -144,7 +144,7 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="<?php echo parse_url(base_url().'assets/bootstrap4/js/bootstrap.min.js', PHP_URL_PATH) ?>" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="<?php echo parse_url(base_url().'assets/bootstrap4/js/bootstrap.min.js', PHP_URL_PATH) ?>"></script>
     <script src="https://unpkg.com/ionicons@4.4.6/dist/ionicons.js"></script>
     <script type="text/javascript">
     	$( "#search" ).focusin(function() {
